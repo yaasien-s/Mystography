eeb28470996e6251ee0c3edbb798c0be961b05b4
yaasien-s <Yaasiensalaam@gmail.com>
2022-03-09 00:45:03 +0200

    addded pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,40 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Explore from '../views/Explore.vue'
+import Register from '../views/Register.vue'
+import Login from '../views/Login.vue'
+import Contact from '../views/Contact.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    name: 'About',
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/explore',
+    name: 'Explore',
+    component: Explore
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
   }
 ]
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,91 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <carousel>
+  <input type="radio" id="page1cb" checked name="pages" />
+  <input type="radio" id="page2cb" name="pages" />
+  <input type="radio" id="page3cb" name="pages" />
+  <input type="radio" id="page4cb" name="pages" />
+  <input type="radio" id="page5cb" name="pages" />
+  <!-- page 1 -->
+  <page id="page1">
+        <img src="@/assets/mystlogo.png" alt="PUPPY">
+    <label for="page2cb" title="Start"><b>Background</b></label>
+  </page>
+  <!-- page 2 -->
+  <page id="page2">
+    <div>Page Two!</div>
+    <label for="page3cb" title="Next"><b>Background</b></label>
+  </page>
+  <!-- page 3 -->
+  <page id="page3">
+    <div>Page Three!</div>
+    <label for="page4cb" title="Next"><b>Background</b></label>
+  </page>
+  <!-- page 4 -->
+  <page id="page4">
+    <div>Page Four!</div>
+    <label for="page5cb" title="Next"><b>Background</b></label>
+  </page>
+  <!-- page 5 -->
+  <page id="page5">
+    <div>Page Five!</div>
+    <label for="page1cb" title="That's all folks!"><b>Background</b></label>
+  </page>
+  </carousel>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+<style lang="scss" scoped>
+carousel {
+  width: 100%;
+  text-align: center;
+  font-family: Trebuchet MS;
+  display: block;
+  font-size: 2rem;
+
+  input {
+    display: none;
+  }
+
+  page {
+    position: absolute;
+    // top: 4em;
+    left: 0;
+    width: 100%;
+    transition: transform 0.5s;
+    transform: scale(0);
+    div {
+      padding: 3em;
+    }
+    img{
+      width: 100%;
+      height: 100vh;
+      object-fit: cover;
+    }
+    label {
+      cursor: pointer;
+      background: #d9ff4f;
+      padding: 10px;
+      position: absolute;
+      width: fit-content;
+      margin-inline: auto;
+    bottom: 2em;
+    left: 0;
+    right: 0;
+    }
+  }
+  @for $i from 1 through 5 {
+    #page#{$i}cb:checked ~ {
+      #page#{$i} {
+        transform: scale(1);
+      }
+    }
+  }
+}
+
+</style>
